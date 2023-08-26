504e66d1d3176b11e3508d8267dd13fbf42d4c26
davidetorre92 <davidetorre92@gmail.com>
2023-08-26 23:05:19 +0200

    GCC slides

--- conference/media/GCC.py
+++ conference/media/GCC.py
@@ -6,9 +6,30 @@ edges_list = [[2,3], [3,4], [4,2],
          [0,2], [1,3],
          [4,5], [4,6]]
 
-G = ig.Graph(n = 7, edges=edges_list)
+G = ig.Graph(n = 7, edges=edges_list, directed = True)
+layout = G.layout(layout='auto')
+visual_style = {}
 
 fig, ax = plt.subplots()
+visual_style['vertex_color'] = ['white' for _ in range(7)]
+ig.plot(G, target=ax, layout=layout, **visual_style)
+fig.savefig("graph_1.pdf")
 
-ig.plot(G, ax=ax)
-fig.show()
+fig, ax = plt.subplots()
+visual_style['vertex_color'][2] = '#440154'
+visual_style['vertex_color'][3] = '#440154'
+visual_style['vertex_color'][4] = '#440154'
+ig.plot(G, target=ax, layout=layout, **visual_style)
+fig.savefig("graph_2.pdf")
+
+fig, ax = plt.subplots()
+visual_style['vertex_color'][0] = '#2A788E'
+visual_style['vertex_color'][1] = '#2A788E'
+ig.plot(G, target=ax, layout=layout, **visual_style)
+fig.savefig("graph_3.pdf")
+
+fig, ax = plt.subplots()
+visual_style['vertex_color'][5] = '#7AD151'
+visual_style['vertex_color'][6] = '#7AD151'
+ig.plot(G, target=ax, layout=layout, **visual_style)
+fig.savefig("graph_4.pdf")
