eb5d9cb49e006cd569d09e04e9f088a106a86a12
davidetorre92 <davidetorre92@gmail.com>
2024-11-18 10:06:03 +0100

    added robustness column

--- visualizations_scripts/tables/article_top_3_most_critical_nodes.py
+++ visualizations_scripts/tables/article_top_3_most_critical_nodes.py
@@ -7,7 +7,7 @@ from utils.data_handling import create_folder
 from settings import top_3_critical_nodes_path, df_critical_nodes_path, df_robustness_path, verbose
 
 N = 3
-print_rho_flag = False
+print_rho_flag = True
 
 create_folder(top_3_critical_nodes_path, verbose = verbose)
 critical_nodes_df = pd.read_pickle(df_critical_nodes_path)
@@ -23,10 +23,10 @@ grouped['Rank'] = range(1, grouped.shape[0] + 1)
 # Order and rename columns
 if print_rho_flag:
     grouped = grouped[['Rank', 'Network', 'name', 'rho_G']]
-    grouped.columns = ['Rank', 'Food web', f"[{','.join([f'$s_{i+1}^G$' for i in range(N)])}]", '$\rho_G$']
+    grouped.columns = ['Rank', 'Food web', f"[{';'.join([f'$s_{i+1}^G$' for i in range(N)])}]", '$\rho_G$']
 else:
     grouped = grouped[['Rank', 'Network', 'name']]
-    grouped.columns = ['Rank', 'Food web', f"[{','.join([f'$s_{i+1}^G$' for i in range(N)])}]"]
+    grouped.columns = ['Rank', 'Food web', f"[{';'.join([f'$s_{i+1}^G$' for i in range(N)])}]"]
 # Generate LaTeX table
 grouped.to_latex(top_3_critical_nodes_path, index = False)
 print(f"LaTeX code successfully saved to {top_3_critical_nodes_path}")
